0bb7c62544312c82e7c45298fb600f0adc6d6652
Jonathan Dunst <johnnt>
2023-06-25 14:19:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//rng
+
+
+
+
+// Spaceship
+
+
+
+//alien array and loop
+
+
+
+//uss ship display
+
+
+
+
+//enenmy display
+
+
+
+
+//turn switch 
+
+
+
+
+//attack func uss & aliens attack
+
+
+
+
+
+//start function
+
+
+
+
+//stats display
+
+
+
+// retreat
